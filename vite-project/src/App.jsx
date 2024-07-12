deb3da0a636e211c8f8843d0ae92b5184abc7bd2
Md Adil Ashrafi <105966902+aadil1206@users.noreply.github.com>
2024-07-13 03:14:48 +0530

    change

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -14,7 +14,7 @@ import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <div className="App">
